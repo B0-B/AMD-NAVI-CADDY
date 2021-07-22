703a81979b23df50cd19b19234f4ae76e919c815
BoB <51753821+B0-B@users.noreply.github.com>
2021-07-22 13:15:36 +0200

    fixed typo in path

--- pull.sh
+++ pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -O - https://b0-b.github.io/amd-driver/setup.sh >> $HOME/setup.sh
-wget -O - https://b0-b.github.io/amd-driver/setup.sh >> $HOME/uninstall.sh
+wget -O - https://b0-b.github.io/amd-driver/uninstall.sh >> $HOME/uninstall.sh
