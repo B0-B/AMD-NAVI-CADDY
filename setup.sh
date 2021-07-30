b2b345544f2bf19f8aec748ea0e07a22c8928d44
BoB <51753821+B0-B@users.noreply.github.com>
2021-07-30 11:30:35 +0200

    removed unnecessary exits.

--- setup.sh
+++ setup.sh
@@ -104,19 +104,17 @@ cd `dirname $0`
 if [ $# -ne 1 ]
 then
     echo "Usage $0: <pci bus id>"
-    exit 1
 fi
 BUSID=$1
 P=`egrep PCI_SLOT_NAME /sys/class/drm/card*/device/uevent | egrep "$BUSID"`
 if [ -z "$P" ]
 then
     echo "Error: no device found for bus id $BUSID, exiting."
-    exit 1
 fi
 DEVDIR=`dirname $P`
 CARD=`echo $DEVDIR | cut -f 5 -d / | sed "s/[^0-9]//g"`
     echo $CARD
-exit 0' >> $dir/miner/teamredminer-v0.8.3-linux/card_from_pci.sh
+' >> $dir/miner/teamredminer-v0.8.3-linux/card_from_pci.sh
 
 
 wait
