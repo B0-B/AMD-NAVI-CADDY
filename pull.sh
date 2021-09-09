9904e13024dbcd94d9a2a8e6509964f8ac11dce1
BoB <51753821+B0-B@users.noreply.github.com>
2021-09-09 19:23:18 +0200

    Update pull.sh

--- pull.sh
+++ pull.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O - https://b0-b.github.io/amd-driver/setup.sh >> $HOME/setup.sh
-wget -O - https://b0-b.github.io/amd-driver/uninstall.sh >> $HOME/uninstall.sh
+wget -O - https://b0-b.github.io/AMD-NAVI-1x-SETUP/setup.sh >> $HOME/setup.sh
+wget -O - https://b0-b.github.io/AMD-NAVI-1x-SETUP/uninstall.sh >> $HOME/uninstall.sh
